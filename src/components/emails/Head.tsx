1c5a3767f9ff258d44eebf42aa134cab20f3f34c
Dawid Janeczko <dawid.j@masterborn.com>
2026-01-09 13:08:38 +0100

    chore: refine fonts linking

--- src/components/emails/Head.tsx
+++ src/components/emails/Head.tsx
@@ -6,13 +6,18 @@ export function Head() {
     <REHead>
       <style>{`html { font-size: 10px; } body { margin: 0; padding-top: 40px; color: ${importedTheme.color.pickledBluewood}; }`}</style>
       <link
-        rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
+        rel="stylesheet"
+        type="text/css"
       />
       <link
-        rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap"
+        rel="stylesheet"
+        type="text/css"
       />
+      <style type="text/css">
+        {`@import url(https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&amp;display=swap); @import url(https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&amp;display=swap);`}
+      </style>
     </REHead>
   );
 }
